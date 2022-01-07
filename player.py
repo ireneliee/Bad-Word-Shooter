1bbba851bc1358b47f86830ea03a447ca209f873
unknown <ireneliee>
2022-01-07 12:09:37 +0800

    add shooting function

--- player.py
+++ player.py
@@ -1,20 +1,18 @@
 import pygame
 from color import Color
 from windowSetting import WindowSetting
-
-WIDTH = 480
-HEIGHT = 600
+from bullet import Bullet
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((50,40))
-        self.image.fill(WindowSetting.GREEN)
+        self.image.fill(Color.GREEN)
         self.speedx = 0
         self.speedy = 0
         self.rect = self.image.get_rect()
-        self.rect.centerx = WIDTH / 2
-        self.rect.bottom = HEIGHT - 10
+        self.rect.centerx = WindowSetting.WIDTH / 2
+        self.rect.bottom = WindowSetting.HEIGHT - 10
         self.speedx = 0
     
     def update(self):
@@ -29,9 +27,14 @@ class Player(pygame.sprite.Sprite):
         self.rect.x += self.speedx
         self.rect.y += self.speedy
 
-        if self.rect.right > WIDTH:
-            self.rect.right = WIDTH
+        if self.rect.right > WindowSetting.WIDTH:
+            self.rect.right = WindowSetting.WIDTH
         
         if self.rect.left < 0:
             self.rect.left = 0
+
+    def shoot(self, all_sprites, bullets):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
 
