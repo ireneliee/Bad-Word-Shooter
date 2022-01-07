1221a0e3c79f9e2f07bb45c59fd639125fd0c7b9
unknown <ireneliee>
2022-01-07 11:12:58 +0800

    remove speedy

--- player.py
+++ player.py
@@ -15,6 +15,8 @@ class Player(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((50,40))
         self.image.fill(GREEN)
+        self.speedx = 0
+        self.speedy = 0
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
@@ -30,6 +32,7 @@ class Player(pygame.sprite.Sprite):
             self.speedx = 5
         
         self.rect.x += self.speedx
+        self.rect.y += self.speedy
 
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
